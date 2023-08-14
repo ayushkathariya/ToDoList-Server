2e33488d6fb5d1c77196488fab73fd25527dad2d
Ayush <ayushkathariyua@ggmail.com>
2023-08-14 19:00:27 +0545

    cors updated

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://to-do-list-client-omega.vercel.app/",
     credentials: true,
   })
 );
