75504f1cf74f466f1287fcda2517ff824b96c1d2
Ayush <ayushkathariyua@ggmail.com>
2023-08-14 18:41:08 +0545

    cors updated

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CORS,
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
